cbd44fa072754a8d10b4c04fa03c16c7eb4438ee
Vyacheslav Bocharov <adeep@lexina.in>
2023-05-19 08:35:20 +0300

    JH: fix sentry

--- supervisor/bootstrap.py
+++ supervisor/bootstrap.py
@@ -322,8 +322,9 @@ def setup_diagnostics(coresys: CoreSys) -> None:
     """Sentry diagnostic backend."""
     _LOGGER.info("Initializing Supervisor Sentry")
     # pylint: disable=abstract-class-instantiated
+    #dsn="https://9c6ea70f49234442b4746e447b24747e@o427061.ingest.sentry.io/5370612",
     sentry_sdk.init(
-        dsn="https://9c6ea70f49234442b4746e447b24747e@o427061.ingest.sentry.io/5370612",
+        dsn="https://47ea2fbffc454c6b877d1d41966873c7@o4505208810110976.ingest.sentry.io/4505208844386304",
         before_send=lambda event, hint: filter_data(coresys, event, hint),
         auto_enabling_integrations=False,
         default_integrations=False,
